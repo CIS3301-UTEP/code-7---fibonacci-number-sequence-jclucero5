2dbd802f86d44bb2bd14ceab40ace224e8611ec2
jclucero5 <Jclucero5@miners.utep.edu>
2024-11-03 16:53:53 -0700

    codeeeeeeeee7.py

--- code_12.py
+++ code_12.py
@@ -3,12 +3,12 @@ def get_fibonacci_number(position):
         return 1
     elif position == 2:
         return 1
-    return get_fibonacci_number(position - 1) + get_fibonacci_number(position + 2)
+    return get_fibonacci_number(position - 1) + get_fibonacci_number(position - 2)
 
 def get_fibonacci_number_sequence(number):
     seq_num = []
     for i in range(1, number + 1):
-        seq_num.append(get_fibonacci_number_sequence(i))
+        seq_num.append(get_fibonacci_number(i))
     return seq_num
 if __name__ == "__main__":
     print(get_fibonacci_number(3))
